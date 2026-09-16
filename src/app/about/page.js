41d47818e57d4f72684c1fdeba2a32f543a14b5f
Agboola Vincent <agboolavin65@gmail.com>
2026-09-16 16:23:40 +0100

    Link and Routing

---
+++ src/app/about/page.js
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div>
+      <main>
+        <h1>About Us</h1>
+        <p>Welcome to our food lover community!</p>
+      </main>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 import styles from "./page.module.css";
 
@@ -5,7 +6,15 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        Hello
+        <p>
+          Hello there! Welcome to our food lover community! We are passionate
+          about sharing delicious recipes, cooking tips, and culinary
+          adventures. Join us on this flavorful journey and let&apos;s explore
+          the world of food together!
+        </p>
+        <p>
+          <Link href="/about">Learn more about us</Link>
+        </p>
       </main>
     </div>
   );
