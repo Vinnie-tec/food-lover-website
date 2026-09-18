bc81825e029272d51d75a6206f35fd2d1b0586a0
Agboola Vincent <agboolavin65@gmail.com>
2026-09-18 10:23:42 +0100

    Created header component and added to the root layout

---
+++ components/main-header.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+import logoImg from '@/assets/logo.png'
+
+import style from './main-header.module.css'
+
+
+export default function MainHeader() {
+  return (
+    <header className={style.header}>
+        <Link className={style.logo} href="/">
+            <Image src={logoImg} alt="A plate with food on it" priority />
+            Nextlevel Food
+        </Link>
+
+        <nav className={style.nav}>
+            <ul>
+                <li><Link href="/meals">BrowseMeals</Link></li>
+                <li><Link href="/community">Foodies Community</Link></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
