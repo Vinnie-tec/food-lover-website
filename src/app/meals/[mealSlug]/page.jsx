3ce0bbff44f998a01baaf081ee46d0549a32b3d1
Agboola Vincent <agboolavin65@gmail.com>
2026-09-18 12:47:52 +0100

    Added code for gettting a single meal by slug

--- src/app/meals/[mealSlug]/page.jsx
+++ src/app/meals/[mealSlug]/page.jsx
@@ -1,7 +1,24 @@
-// localhost:3000/meals/meal-1
+import style from "./page.module.css";
 
 const MealsDetailsPage = () => {
-  return <div>Meals Details Page</div>;
+  return (
+    <>
+      <header className={style.header}>
+        <div className={style.image}>{/* <Image fill /> */}</div>
+        <div className={style.headerText}>
+          <h1>Meal Title</h1>
+          <p className={style.creator}>
+            by <a href={`mailto:${"chefEmail"}`}>Chef Name</a>
+          </p>
+          <p className={style.summary}>Brief description of the meal.</p>
+        </div>
+      </header>
+      <main
+        className={style.instructions}
+        dangerouslySetInnerHTML={{ __html: "... " }}
+      ></main>
+    </>
+  );
 };
 
 export default MealsDetailsPage;
